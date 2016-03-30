ece6c655b24f9a2a8f89961b99875262a325bc6b
owolp <zkostov@protonmail.com>
2016-03-30 16:46:05 +0300

    Introduction to Programming HWs

---
+++ Modul I/CSharp-Part-1/01-Introduction-to-Programming/04.HelloCSharp/HelloCSharp.cs
@@ -0,0 +1,24 @@
+﻿//04. Hello World
+//Description
+//Create, compile and run a Hello World application - it should print the string Hello, C#! on the console.
+//Input
+//    There is no input for this task.
+//Output
+//    Print Hello, C#! on the console.
+//Constraints
+//    Time limit: 0.1s
+//    Memory limit: 16MB
+
+using System;
+
+namespace HelloCSharp
+{
+    class HelloCSharp
+    {
+        static void Main()
+        {
+            var helloCSharp = "Hello, C#!";
+            Console.WriteLine(helloCSharp);
+        }
+    }
+}
