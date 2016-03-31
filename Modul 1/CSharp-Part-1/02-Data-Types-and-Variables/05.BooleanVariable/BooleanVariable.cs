349a16cd21efe4cc14f7f200445f2a52bb14e4dc
owolp <zkostov@protonmail.com>
2016-03-31 15:56:49 +0300

    Data Types and Variables HWs

---
+++ Modul 1/CSharp-Part-1/02-Data-Types-and-Variables/05.BooleanVariable/BooleanVariable.cs
@@ -0,0 +1,17 @@
+﻿//05. Boolean Variable
+//Description
+//Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender.Print it on the console.
+
+using System;
+
+namespace BooleanVariable
+{
+    class BooleanVariable
+    {
+        static void Main()
+        {
+            bool isFemale = false;
+            Console.WriteLine(isFemale);
+        }
+    }
+}
---
+++ Modul 1/CSharp-Part-1/02-Data-Types-and-Variables/09.ExchangeVariableValues/ExchangeVariableValues.cs
@@ -0,0 +1,26 @@
+﻿//09. Exchange Variable Values
+//Description
+//Declare two integer variables a and b and assign them with 5 and 10 and after that exchange their values by using some programming logic.Print the variable values before and after the exchange.
+
+using System;
+
+namespace ExchangeVariableValues
+{
+    class ExchangeVariableValues
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+        }
+    }
+}
