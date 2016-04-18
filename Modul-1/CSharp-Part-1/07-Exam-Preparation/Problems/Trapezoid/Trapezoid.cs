cf2d3b0f48f2baa7db813c42cdb7554a5fb08f40
owolp <zkostov@protonmail.ch>
2016-04-18 10:30:58 +0300

    Upload Trapezoid

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/Trapezoid/Trapezoid.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Trapezoid
+{
+    class Trapezoid
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //..........**********
+            Console.Write(new string('.', n));
+            Console.WriteLine(new string('*', n));
+
+            //.........*.........*
+            //........*..........*
+            //.......*...........*
+            //......*............*
+            //.....*.............*
+            //....*..............*
+            //...*...............*
+            //..*................*
+            //.*.................*
+            for (int row = 0; row <= n - 2; row++)
+            {
+                Console.Write(new string('.', n - 1 - row));
+                Console.Write('*');
+                Console.Write(new string('.', n - 1 + row));
+                Console.WriteLine('*');
+            }
+
+            //********************
+            Console.WriteLine(new string('*', 2 * n));
+        }
+    }
+}
