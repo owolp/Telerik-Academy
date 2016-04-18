414d9cf395e7042f590ede3365ea94e5c0520cac
owolp <zkostov@protonmail.ch>
2016-04-18 20:52:50 +0300

    Upload Money

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/Money/Money.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Money
+{
+    class Money
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            int paperPerStudent = n * s;
+            double realmsNeeded = (double)paperPerStudent / 400;
+            double realmsPrice = realmsNeeded * p;
+
+            Console.WriteLine("{0:F3}", realmsPrice);
+        }
+    }
+}
