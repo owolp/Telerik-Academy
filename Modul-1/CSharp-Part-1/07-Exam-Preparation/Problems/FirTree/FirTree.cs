c745c65d394c7e64fb4e3e84d58b1635f9ae91fd
owolp <zkostov@protonmail.ch>
2016-04-18 10:42:54 +0300

    Upload FirTree

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/FirTree/FirTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirTree
+{
+    class FirTree
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //.......*.......
+            //......***......
+            //.....*****.....
+            //....*******....
+            //...*********...
+            //..***********..
+            //.*************.
+            //***************
+            for (int row = 0, index = 1; row < n - 1; row++, index += 2)
+            {
+                Console.Write(new string('.', n - 2 - row));
+                Console.Write(new string('*', index));
+                Console.WriteLine(new string('.', n - 2 - row));
+            }
+
+            //.......*.......
+            Console.Write(new string('.', n - 2));
+            Console.Write('*');
+            Console.WriteLine(new string('.', n - 2));
+        }
+    }
+}
