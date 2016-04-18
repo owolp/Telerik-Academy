cc4fe0ed305983b3d255d8247b3df555eaa28f74
owolp <zkostov@protonmail.ch>
2016-04-18 13:59:10 +0300

    Upload UKFlag

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/UKFlag/UKFlag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace UKFlag
+{
+    class UKFlag
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 0; row < n / 2; row++)
+            {
+                Console.Write(new string('.', row));
+                Console.Write('\\');
+                Console.Write(new string('.', (n / 2 - row) - 1));
+                Console.Write('|');
+                Console.Write(new string('.', (n / 2 - row) - 1));
+                Console.Write('/');
+                Console.WriteLine(new string('.', row));
+            }
+
+            Console.Write(new string('-', n / 2));
+            Console.Write('*');
+            Console.WriteLine(new string('-', n / 2));
+
+            for (int row = 0; row < n / 2; row++)
+            {
+                Console.Write(new string('.', (n / 2 - row) - 1));
+                Console.Write('/');
+                Console.Write(new string('.', row));
+                Console.Write('|');
+                Console.Write(new string('.',row));
+                Console.Write('\\');
+                Console.WriteLine(new string('.', (n / 2 - row) - 1));
+            }
+        }
+    }
+}
