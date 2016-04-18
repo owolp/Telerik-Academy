4aa2e19416d79fa547d636b14cda987442f8405c
owolp <zkostov@protonmail.ch>
2016-04-18 10:21:19 +0300

    Upload ForestRoad

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/ForestRoad/ForestRoad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ForestRoad
+{
+    class ForestRoad
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //*....
+            //.*...
+            //..*..
+            //...*.
+            //....*
+            for (int row = 0; row < n; row++)
+            {
+                Console.Write(new string('.', row));
+                Console.Write('*');
+                Console.WriteLine(new string('.', n - 1 - row));
+            }
+
+            //...*.
+            //..*..
+            //.*...
+            //*....
+            for (int row = 1; row <= n - 1; row++)
+            {
+                Console.Write(new string('.', n - 1 - row));
+                Console.Write('*');
+                Console.WriteLine(new string('.', row));
+            }
+
+        }
+    }
+}
