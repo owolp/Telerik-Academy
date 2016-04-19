0195b4d8794d02d88750d97b3af3e1f543ff5b32
owolp <zkostov@protonmail.ch>
2016-04-19 16:10:14 +0300

    Upload Sevenland Numbers

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/SevenlandNumbers/SevenlandNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SevenlandNumbers
+{
+    class SevenlandNumbers
+    {
+        static void Main()
+        {
+            int k = int.Parse(Console.ReadLine());
+            int result = k + 1;
+
+            int firstDigit = k % 10;
+            k /= 10;
+            int secondDigit = k % 10;
+            k /= 10;
+            int thirdDigit = k % 10;
+
+            if (firstDigit == 6)
+            {
+                firstDigit = 0;
+                secondDigit++;
+                if (secondDigit == 7)
+                {
+                    secondDigit = 0;
+                    thirdDigit++;
+                    if (thirdDigit == 7)
+                    {
+                        thirdDigit = 10;
+                    }
+                }
+                result = 1 * firstDigit + 10 * secondDigit + 100 * thirdDigit;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
