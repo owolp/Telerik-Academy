ad4d8f4a5d616e2fe73946c6fe2465df1586ad4f
owolp <zkostov@protonmail.ch>
2016-04-19 14:35:51 +0300

    Update Next Date

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/NextDate/NextDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NextDate
+{
+    class NextDate
+    {
+        static void Main()
+        {
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+
+            DateTime date = new DateTime(year, month, day);
+            date = date.AddDays(1);
+
+            Console.WriteLine("{0:d.M.yyyy}", date);
+        }
+    }
+}
