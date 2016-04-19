6aa8487a3ebe6022b57f1678d7d60af08756472c
owolp <zkostov@protonmail.ch>
2016-04-19 10:00:23 +0300

    Upload Printing

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/Printing/Printing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Printing
+{
+    class Printing
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int s = int.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            int totalPapers = s * n;
+            double totalRealms = (double)totalPapers / 500;
+            double priceRealms = totalRealms * p;
+
+            Console.WriteLine("{0:F2}", priceRealms);
+        }
+    }
+}
