5da90c42f430b920ac6eb7f1390acb1bcf89e7bc
owolp <zkostov@protonmail.ch>
2016-04-19 15:10:58 +0300

    Upload Triple Rotation of Digits

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/TripleRotationOfDigits/TripleRotationOfDigits.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TripleRotationOfDigits
+{
+    class TripleRotationOfDigits
+    {
+        static void Main()
+        {
+            int k = int.Parse(Console.ReadLine());
+
+            int result = 0;
+
+            if (k < 10)
+            {
+                Console.WriteLine(k);
+            }
+            else
+            {
+                int lastDigit = k % 10;
+                k /= 10;
+                string kAsString = Convert.ToString(k);
+                kAsString = lastDigit + kAsString;
+                result = int.Parse(kAsString);
+
+                lastDigit = result % 10;
+                result /= 10;
+                kAsString = Convert.ToString(result);
+                kAsString = lastDigit + kAsString;
+                result = int.Parse(kAsString);
+
+                lastDigit = result % 10;
+                result /= 10;
+                kAsString = Convert.ToString(result);
+                if (kAsString == "0")
+                {
+                    result = lastDigit;
+                }
+                else
+                {
+                    kAsString = lastDigit + kAsString;
+                    result = int.Parse(kAsString);
+                }
+
+
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
