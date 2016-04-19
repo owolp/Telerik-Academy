94ec02df7fe9d088b81befe455bdff03ad8e7c5e
owolp <zkostov@protonmail.ch>
2016-04-19 13:32:29 +0300

    Upload 369

--- Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/369/369.cs
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/369/369.cs
@@ -1,27 +1,27 @@
 ﻿using System;
 
-namespace _248
+namespace _369
 {
-    class _248
+    class _369
     {
         static void Main()
         {
             long a = long.Parse(Console.ReadLine());
-            uint secret = uint.Parse(Console.ReadLine());
+            long code = int.Parse(Console.ReadLine());
             long c = long.Parse(Console.ReadLine());
 
             long r = 0;
 
-            switch (secret)
+            switch (code)
             {
-                case 2:
-                    r = a % c;
-                    break;
-                case 4:
+                case 3:
                     r = a + c;
                     break;
-                case 8:
+                case 6:
                     r = a * c;
+                    break;
+                case 9:
+                    r = a % c;
                     break;
                 default:
                     return;
@@ -29,13 +29,13 @@ namespace _248
 
             double remainder = 0;
 
-            if (r % 4 == 0)
+            if (r % 3 == 0)
             {
-                remainder = (double)r / 4;
+                remainder = r / 3;
             }
             else
             {
-                remainder = r % 4;
+                remainder = r % 3;
             }
 
             Console.WriteLine(remainder);
