bc0dd64878b0f1963dfc06df936e2ccd5be55669
owolp <zkostov@protonmail.ch>
2016-04-19 13:23:36 +0300

    Upload 248

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/248/248.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _248
+{
+    class _248
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            uint secret = uint.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            long r = 0;
+
+            switch (secret)
+            {
+                case 2:
+                    r = a % c;
+                    break;
+                case 4:
+                    r = a + c;
+                    break;
+                case 8:
+                    r = a * c;
+                    break;
+                default:
+                    return;
+            }
+
+            double remainder = 0;
+
+            if (r % 4 == 0)
+            {
+                remainder = (double)r / 4;
+            }
+            else
+            {
+                remainder = r % 4;
+            }
+
+            Console.WriteLine(remainder);
+            Console.WriteLine(r);
+        }
+    }
+}
