5a3f413e767bf806aad7a5ac66ec8085c1acceb0
owolp <zkostov@protonmail.ch>
2016-04-19 13:46:37 +0300

    Upload Peace Of Cake

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/PeaceOfCake/PeaceOfCake.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PeaceOfCake
+{
+    class PeaceOfCake
+    {
+        static void Main()
+        {
+            long a = long.Parse(Console.ReadLine());
+            long b = long.Parse(Console.ReadLine());
+            long c = long.Parse(Console.ReadLine());
+            long d = long.Parse(Console.ReadLine());
+
+            if (b != d)
+            {
+                a *= d;
+                c *= b;
+            }
+
+            decimal sum = (decimal)(a + c) / (b * d);
+
+            if (sum > 1)
+            {
+                Console.WriteLine((long)sum);
+            }
+            else
+            {
+                Console.WriteLine("{0:F22}", sum);
+            }
+
+            Console.WriteLine((a + c) + "/" + (b * d));
+        }
+    }
+}
