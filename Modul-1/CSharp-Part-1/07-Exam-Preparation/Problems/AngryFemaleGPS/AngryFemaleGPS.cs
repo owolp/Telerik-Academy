7d6b4ab453b793025c740260b7f1e983fcc52652
owolp <zkostov@protonmail.ch>
2016-04-20 18:07:59 +0300

    Upload Angry Female GPS

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/AngryFemaleGPS/AngryFemaleGPS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace AngryFemaleGPS
+{
+    class AngryFemaleGPS
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            long n = long.Parse(input);
+            n = Math.Abs(n);
+
+            long evenSum = 0;
+            long oddSum = 0;
+
+            while (n > 0)
+            {
+                var number = n % 10;
+
+                if (number % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+
+                n /= 10;
+            }
+
+            string direction;
+            long sum = 0;
+
+            if (evenSum > oddSum)
+            {
+                direction = "right";
+                sum = evenSum;
+            }
+            else if (oddSum > evenSum)
+            {
+                direction = "left";
+                sum = oddSum;
+            }
+            else
+            {
+                direction = "straight";
+                sum = evenSum;
+            }
+
+            Console.WriteLine(direction + " " + sum);
+        }
+    }
+}
