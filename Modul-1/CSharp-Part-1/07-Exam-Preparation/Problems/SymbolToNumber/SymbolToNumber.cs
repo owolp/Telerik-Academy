e813ce6c3819999d40a878190ba6ab123e8bab3a
owolp <zkostov@protonmail.ch>
2016-04-20 10:31:39 +0300

    Upload Symbol To Number

---
+++ Modul-1/CSharp-Part-1/07-Exam-Preparation/Problems/SymbolToNumber/SymbolToNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace SymbolToNumber
+{
+    class SymbolToNumber
+    {
+        static void Main()
+        {
+            int secret = int.Parse(Console.ReadLine());
+
+            var text = Console.ReadLine();
+
+            int position = 0;
+            char ch = text[position];
+
+            while (true)
+            {
+                double result = 0;
+
+                if (ch == '@')
+                {
+                    break;
+                }
+                else if (char.IsLetter(ch))
+                {
+                    result = (ch * secret) + 1000;
+                }
+                else if (char.IsDigit(ch))
+                {
+                    result = ch + secret + 500;
+                }
+                else
+                {
+                    result = ch - secret;
+                }
+
+                if (position % 2 == 0)
+                {
+                    result /= 100;
+                    Console.WriteLine("{0:F2}", result);
+                }
+                else
+                {
+                    result *= 100;
+                    Console.WriteLine(result);
+                }
+
+                position++;
+                ch = text[position];
+            }
+
+        }
+    }
+}
