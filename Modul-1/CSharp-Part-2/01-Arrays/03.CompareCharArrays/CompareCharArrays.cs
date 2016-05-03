069dee60dfbdc9597650cf794d870a7636b0be1b
owolp <zkostov@protonmail.ch>
2016-05-03 19:37:25 +0300

    Update Arrays HW

---
+++ Modul-1/CSharp-Part-2/01-Arrays/03.CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CompareCharArrays
+{
+    class CompareCharArrays
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine();
+            char[] firstArray = input.ToCharArray();
+
+            input = Console.ReadLine();
+            char[] secondArray = input.ToCharArray();
+
+            char symbol = 'a';
+
+            int length = Math.Min(firstArray.Length, secondArray.Length);
+
+            for (int i = 0; i < length; i++)
+            {
+                if (firstArray.Length > secondArray.Length)
+                {
+                    symbol = '>';
+                    break;
+                }
+                else if (firstArray.Length < secondArray.Length)
+                {
+                    symbol = '<';
+                    break;
+                }
+                else
+                {
+                    if (firstArray[i] < secondArray[i])
+                    {
+                        symbol = '<';
+                    }
+                    else if (firstArray[i] > secondArray[i])
+                    {
+                        symbol = '>';
+                    }
+                    else
+                    {
+                        symbol = '=';
+                    }
+                }
+            }
+
+            Console.WriteLine(symbol);
+        }
+    }
+}
---
+++ Modul-1/CSharp-Part-2/01-Arrays/04.MaximalSequence/MaximalSequence.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MaximalSequence
+{
+    class MaximalSequence
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int counter = 1;
+            int maxSequence = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    counter++;
+                    if (counter > maxSequence)
+                    {
+                        maxSequence = counter;
+                    }
+                }
+                else
+                {
+                    counter = 1;
+                }
+            }
+
+            Console.WriteLine(maxSequence);
+        }
+    }
+}
