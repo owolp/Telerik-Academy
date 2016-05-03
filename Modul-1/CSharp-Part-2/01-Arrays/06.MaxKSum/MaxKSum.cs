1c61099e98bfe4fa8730433cc0e038b64eb1c71f
owolp <zkostov@protonmail.ch>
2016-05-03 23:00:16 +0300

    Update Arrays HW

---
+++ Modul-1/CSharp-Part-2/01-Arrays/06.MaxKSum/MaxKSum.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MaxKSum
+{
+    class MaxKSum
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            int sum = 0;
+
+            for (int i = n - 1; i >= n - k; i--)
+            {
+                sum += numbers[i];
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Modul-1/CSharp-Part-2/01-Arrays/07.SelectionSort/SelectionSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (numbers[j] < numbers[i])
+                    {
+                        var temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
