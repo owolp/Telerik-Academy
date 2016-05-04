9199a11eea141a19693596ee7150b3903d6dacc5
owolp <zkostov@protonmail.ch>
2016-05-04 18:35:58 +0300

    Upload BinarySearch

---
+++ Modul-1/CSharp-Part-2/01-Arrays/11.BinarySearch/BinarySearch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class BinarySearch
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int x = int.Parse(Console.ReadLine());
+
+            int position = -1;
+
+            int start = 0;
+            int end = n - 1;
+
+            while (start <= end)
+            {
+                int middle = (start + end) / 2;
+
+                if (x > numbers[middle])
+                {
+                    start = middle + 1;
+                }
+                else if (x < numbers[middle])
+                {
+                    end = middle - 1; 
+                }
+                else // x = middle
+                {
+                    position = middle;
+                    break;
+                }
+            }
+
+            Console.WriteLine(position);
+        }
+    }
+}
