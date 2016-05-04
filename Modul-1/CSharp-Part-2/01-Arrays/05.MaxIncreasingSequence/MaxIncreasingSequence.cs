70e0d00f5c39332f1bf263f1166f6000e3fa72fe
owolp <zkostov@protonmail.ch>
2016-05-04 15:30:27 +0300

    Update HW

---
+++ Modul-1/CSharp-Part-2/01-Arrays/05.MaxIncreasingSequence/MaxIncreasingSequence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MaxIncreasingSequence
+{
+    class MaxIncreasingSequence
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int currentIncSeq = numbers[0];
+            int maxIncSeq = 0;
+
+            int currentCounter = 1;
+            int maxCounter = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[i] > numbers[i - 1])
+                {
+                    currentIncSeq += numbers[i];
+
+                    if (currentIncSeq > maxIncSeq)
+                    {
+                        maxIncSeq = currentIncSeq;
+                        currentCounter++;
+                        if (currentCounter > maxCounter)
+                        {
+                            maxCounter = currentCounter;
+                        }
+                    }
+                }
+                else
+                {
+                    currentIncSeq = numbers[i];
+                    maxIncSeq = 0;
+                    currentCounter = 1;
+                }
+            }
+            Console.WriteLine(maxCounter);
+        }
+    }
+}
---
+++ Modul-1/CSharp-Part-2/01-Arrays/08.MaxSum/MaxSum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MaxSum
+{
+    class MaxSum
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int bestSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                var currentSum = 0;
+                for (int j = i; j < n; j++)
+                {
+                    currentSum += numbers[j];
+                    if (currentSum > bestSum)
+                    {
+                        bestSum = currentSum;
+                    }
+                }
+            }
+
+            Console.WriteLine(bestSum);
+        }
+    }
+}
