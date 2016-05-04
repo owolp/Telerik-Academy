1c5a95aef8295f14724b06c19bbd11505d688ef6
owolp <zkostov@protonmail.ch>
2016-05-04 13:32:17 +0300

    Update FrequentNumber

---
+++ Modul-1/CSharp-Part-2/01-Arrays/09.FrequentNumber/FrequentNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FrequentNumber
+{
+    class FrequentNumber
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers);
+
+            int number = numbers[0];
+            int count = 1;
+            int maxCount = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    count++;
+
+                    if (count > maxCount)
+                    {
+                        maxCount = count;
+                        number = numbers[i];
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }                
+            }
+
+            Console.WriteLine("{0} ({1} times)", number, maxCount);
+        }
+    }
+}
