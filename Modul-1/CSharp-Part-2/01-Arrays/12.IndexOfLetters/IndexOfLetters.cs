b39f4a82456595987b8fde23224dfcb510d346c8
owolp <zkostov@protonmail.ch>
2016-05-06 12:20:08 +0300

    Upload IndexOfLetters

---
+++ Modul-1/CSharp-Part-2/01-Arrays/12.IndexOfLetters/IndexOfLetters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IndexOfLetters
+{
+    class IndexOfLetters
+    {
+        static void Main()
+        {
+            char[] alphabet = new char[26];
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                alphabet[i] = (char)(97 + i);
+            }
+
+            var input = Console.ReadLine();
+
+            char[] inputArr = input.ToCharArray();
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (inputArr[i] == alphabet[j])
+                    {
+                        Console.WriteLine(j);
+                    }
+                }
+            }
+        }
+    }
+}
