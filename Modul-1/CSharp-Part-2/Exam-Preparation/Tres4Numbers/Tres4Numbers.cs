0888a0739d7f74a5a88824cd95ab0494935c5467
owolp <zkostov@protonmail.ch>
2016-05-17 15:03:15 +0300

    Upload Tres4Numbers

---
+++ Modul-1/CSharp-Part-2/Exam-Preparation/Tres4Numbers/Tres4Numbers.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tres4Numbers
+{
+    class Tres4Numbers
+    {
+        static void Main()
+        {
+            ulong input = ulong.Parse(Console.ReadLine());
+
+            ulong outBase = 9;
+
+            if (input == 0)
+            {
+                Console.WriteLine(Dictionary['0']);
+            }
+            else
+            {
+                Console.WriteLine(BaseToSpecial(DecToBase(outBase, input)));
+            }      
+        }
+
+        static Dictionary<char, string> Dictionary = new Dictionary<char, string>()
+        {
+            { '0' , "LON+" },
+            { '1' , "VK-" },
+            { '2' , "*ACAD" },
+            { '3' , "^MIM" },
+            { '4' , "ERIK|" },
+            { '5' , "SEY&" },
+            { '6' , "EMY>>" },
+            { '7' , "/TEL" },
+            { '8' , "<<DON" },
+        };
+
+        static string DecToBase(ulong @base, ulong decimalNumber)
+        {
+            string baseNumber = string.Empty;
+
+            while (decimalNumber > 0)
+            {
+                ulong digit = decimalNumber % @base;
+
+                if (0 <= digit && digit <= @base)
+                {
+                    baseNumber = (char)digit - (char)0 + baseNumber;
+                }
+
+                decimalNumber /= @base;
+            }
+
+            return baseNumber;
+        }
+
+        static StringBuilder BaseToSpecial(string baseNumber)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < baseNumber.Length; i++)
+            {
+                sb.Append(Dictionary[baseNumber[i]]);
+            }
+
+            return sb;
+        }
+    }
+}
