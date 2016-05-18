60abf37addd495c370fdbc8a440f9052eb971748
owolp <zkostov@protonmail.ch>
2016-05-18 11:25:19 +0300

    Upload IncreasingAbsoluteDifferences.cs

---
+++ Modul-1/CSharp-Part-2/Exam-Preparation/IncreasingAbsoluteDifferences/IncreasingAbsoluteDifferences.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace IncreasingAbsoluteDifferences
+{
+    class IncreasingAbsoluteDifferences
+    {
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine((FindSeqOfAbsDiff(ReadArray())));
+            }
+        }
+
+        static int[] ReadArray()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            return numbers;
+        }
+
+        static bool FindSeqOfAbsDiff(int[] numbers)
+        {
+            bool answer = true;
+
+            int[] seqArr = new int[numbers.Length - 1];
+
+            for (int i = 0; i < seqArr.Length; i++)
+            {
+                seqArr[i] = Math.Abs(numbers[i] - numbers[i + 1]);
+            }
+
+            answer = FindIncSeq(seqArr);
+
+            return answer;
+        }
+
+        static bool FindIncSeq(int[] seqArr)
+        {
+            bool answer = true;
+
+            for (int i = 1; i < seqArr.Length; i++)
+            {
+                int absDiff = seqArr[i] - seqArr[i - 1];
+
+                if (0 > absDiff || absDiff > 1)
+                {
+                    return answer = false;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
