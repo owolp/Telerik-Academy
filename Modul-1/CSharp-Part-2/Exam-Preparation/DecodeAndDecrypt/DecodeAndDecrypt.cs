e6afa5e31f169a1b708186bce2f28801cef161f6
owolp <zkostov@protonmail.ch>
2016-05-30 17:05:01 +0300

    Update Ezam Prep Tasks

---
+++ Modul-1/CSharp-Part-2/Exam-Preparation/DecodeAndDecrypt/DecodeAndDecrypt.cs
@@ -0,0 +1,72 @@
+﻿// Partial
+
+namespace DecodeAndDecrypt
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class DecodeAndDecrypt
+    {
+        static void Main()
+        {
+            var encryptedMessage = Console.ReadLine();
+            char[] encMsgList = encryptedMessage.ToCharArray();
+
+            // lengthOfCypher
+            int cypherLength = encMsgList[encMsgList.Length - 1] - '0';
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = encryptedMessage.Length - 1 - cypherLength; i < encryptedMessage.Length - 1; i++)
+            {
+                sb.Append(encryptedMessage[i]);
+            }
+
+            // cypher
+            var cypher = Convert.ToString(sb);
+            sb.Clear();
+
+            for (int i = 0; i < encMsgList.Length - cypherLength - 1 ; i++)
+            {
+                sb.Append(encMsgList[i]);
+            }
+
+            // Encrypt(message, cypher)
+            var encryption = "4a3b4caa";
+            //var encryption = sb.ToString();
+            sb.Clear();
+
+            int position = 0;
+            while (true)
+            {
+                if (position == encryption.Length)
+                {
+                    break;
+                }
+                char currentDigit = encryption[position];
+                if (char.IsLetter(currentDigit))
+                {
+                    sb.Append(currentDigit);
+                    position++;
+                }
+                else
+                {
+                    for (int i = 0; i < currentDigit - '0'; i++)
+                    {
+                        sb.Append(encryption[position + 1]);
+                    }
+                    position += 2;
+                }
+            }
+
+            string encodedText = sb.ToString();
+
+
+
+
+
+        }
+    }
+}
---
+++ Modul-1/CSharp-Part-2/Exam-Preparation/GreedyDwarf/GreedyDwarf.cs
@@ -0,0 +1,82 @@
+﻿// PARTIAL
+
+namespace GreedyDwarf
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class GreedyDwarf
+    {
+        static void Main()
+        {
+            int[] valley = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int m = int.Parse(Console.ReadLine());
+
+            int maxCoints = 0;
+
+            for (int i = 0; i < m; i++)
+            {
+                int[] patterns = Console.ReadLine().Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                int[] copiedValley = new int[valley.Length];
+                Array.Copy(valley, copiedValley, valley.Length);
+
+                int currentPosition = 0;
+                int currentNumber = patterns[currentPosition];
+                int currentCoints = 0;
+
+
+                int pattern = 0;
+                while (true)
+                {
+
+                    if (currentNumber == 0 || currentPosition + patterns.Length > copiedValley.Length)
+                    {
+                        if (currentCoints > maxCoints)
+                        {
+                            maxCoints = currentCoints;
+                        }
+                        break;
+                    }
+
+                    currentCoints += copiedValley[currentPosition];
+                    copiedValley[currentPosition] = 0;
+
+                    int nextPosition = 0;
+
+                    if (pattern >= patterns.Length)
+                    {
+                        pattern = 0;
+                    }
+
+                    int positionsToMove = patterns[pattern];
+                    nextPosition = currentPosition + positionsToMove;
+
+                    if (copiedValley[nextPosition] == 0)
+                    {
+                        if (currentCoints > maxCoints)
+                        {
+                            maxCoints = currentCoints;
+                        }
+                        break;
+                    }
+
+                    currentCoints += copiedValley[nextPosition];
+                    copiedValley[nextPosition] = 0;
+
+                    currentPosition = nextPosition;
+
+                    pattern++;
+                }
+            }
+
+
+
+            Console.WriteLine(maxCoints);
+        }
+    }
+}
