accf4f144b2ffeca9ddce97d44f0a0d303b0a0e9
owolp <zkostov@protonmail.ch>
2016-06-27 14:18:06 +0300

    Upload MaxSum

---
+++ Modul-1/JavaScript-Fundamentals/Exam-Preparation/MaxSum/app.js
@@ -0,0 +1,41 @@
+function solve(input) {
+    var i, j, k, length, maxSum,
+        currentSum = 0;
+
+    input = input.slice(1).map(Number);
+
+    maxSum = input[0];
+
+    length = input.length;
+    for (i = 0; i <= length; i += 1) {
+        for (j = i; j <= length; j += 1) {
+            for (k = i; k <= j; k += 1) {
+                currentSum += input[k];
+            }
+
+            if (currentSum > maxSum) {
+                maxSum = currentSum;
+            }
+
+            currentSum = 0;
+        }
+    }
+
+    console.log(maxSum);
+}
+
+input = [
+    8, 1, 6, -9, 4, 4, -2, 10, -1
+];
+
+input2 = [
+    6, 1, 3, -5, 8, 7, -6
+];
+
+input3 = [
+    9, -9, -8, -8, -7, -6, -5, -1, -7, -6
+];
+
+//solve(input);
+//solve(input2);
+//solve(input3);
--- Modul-1/JavaScript-Fundamentals/Exam-Preparation/README.md
+++ Modul-1/JavaScript-Fundamentals/Exam-Preparation/README.md
@@ -7,5 +7,5 @@
 | 13 June 2015 Re-Test | MinMax | Triathlon | N/A | N/A |
 | 19 May 2014 | Vehicles | Paths | Shaver Parser | Coord System |
 | 20 May 2014 | Cakes | Horsy | NikolAngular JS | Catalan Numbers |
-| 1 April 2013 Evening | Max Sum | Labyrinth Escape | Listy | N/A |
-| [1 April 2013 Morning](http://bgcoder.com/Contests/Practice/Index/74#0) | [Sequences](./Sequences) | Joro the Naughty | Clojure Parsing | N/A |
+| [1 April 2013 Evening](http://bgcoder.com/Contests/Practice/Index/75) | [Max Sum](./MaxSum) | Labyrinth Escape | Listy | N/A |
+| [1 April 2013 Morning](http://bgcoder.com/Contests/Practice/Index/74) | [Sequences](./Sequences) | Joro the Naughty | Clojure Parsing | N/A |
