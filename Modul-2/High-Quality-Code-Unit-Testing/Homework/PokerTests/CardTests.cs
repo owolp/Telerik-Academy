dcef85dea381b7658029e58cf4662314a88a7722
owolp <zkostov@protonmail.ch>
2016-07-23 18:25:43 +0300

    Upload TDT up to Task 5.

---
+++ Modul-2/High-Quality-Code-Unit-Testing/Homework/PokerTests/CardTests.cs
@@ -0,0 +1,38 @@
+﻿namespace PokerTests
+{
+    using NUnit.Framework;
+    using Poker;
+
+    [TestFixture]
+    public class CardTests
+    {
+        //[Test, Ignore("Just a test to show how to ignore Tests with Nunit")]
+        //public void TestShouldBeIgnored()
+        //{
+        //}
+
+        [Test]
+        public void Card_ToStringMethodShouldReturnNoExceptions()
+        {
+            var card = new Card(CardFace.Ace, CardSuit.Clubs);
+
+            Assert.AreEqual(string.Format($"{CardFace.Ace} of {CardSuit.Clubs}"), card.ToString());
+        }
+
+        [Test]
+        public void Card_ShouldNotBeEqual_WhenThereIsEmptySpaceBeforeTheCardFace()
+        {
+            var card = new Card(CardFace.Ace, CardSuit.Clubs);
+
+            Assert.AreNotEqual(string.Format($" {CardFace.Ace} of {CardSuit.Clubs}"), card.ToString());
+        }
+
+        [Test]
+        public void Card_ShouldNotBeEqual_WhenThereIsEmptySpaceAfterTheCardFace()
+        {
+            var card = new Card(CardFace.Ace, CardSuit.Clubs);
+
+            Assert.AreNotEqual(string.Format($"{CardFace.Ace} of {CardSuit.Clubs} "), card.ToString());
+        }
+    }
+}
---
+++ Modul-2/High-Quality-Code-Unit-Testing/Homework/PokerTests/HandTests.cs
@@ -0,0 +1,61 @@
+﻿namespace PokerTests
+{
+    using System.Collections.Generic;
+    using NUnit.Framework;
+    using Poker;
+
+    [TestFixture]
+    public class HandTests
+    {
+        [Test]
+        public void Hand_ToStringMethodShouldReturnNoExceptions()
+        {
+            var firstCard = new Card(CardFace.Ace, CardSuit.Clubs);
+            var secondCard = new Card(CardFace.Eight, CardSuit.Diamonds);
+            var expectedResult = string.Format($"{CardFace.Ace} of {CardSuit.Clubs}, {CardFace.Eight} of {CardSuit.Diamonds}");
+
+            var listOfCard = new List<ICard>();
+            listOfCard.Add(firstCard);
+            listOfCard.Add(secondCard);
+
+            var hand = new Hand(listOfCard);
+            var actualResult = hand.ToString();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Hand_ShouldNotBeEqual_WhenThereIsNoEmptySpaceBetweenTheCards()
+        {
+            var firstCard = new Card(CardFace.Ace, CardSuit.Clubs);
+            var secondCard = new Card(CardFace.Eight, CardSuit.Diamonds);
+            var expectedResult = string.Format($"{CardFace.Ace} of {CardSuit.Clubs},{CardFace.Eight} of {CardSuit.Diamonds}");
+
+            var listOfCard = new List<ICard>();
+            listOfCard.Add(firstCard);
+            listOfCard.Add(secondCard);
+
+            var hand = new Hand(listOfCard);
+            var actualResult = hand.ToString();
+
+            Assert.AreNotEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void Hand_ShouldNotBeEqual_WhenThereIsNoCommaSpaceBetweenTheCards()
+        {
+            var firstCard = new Card(CardFace.Ace, CardSuit.Clubs);
+            var secondCard = new Card(CardFace.Eight, CardSuit.Diamonds);
+            var expectedResult = string.Format($"{CardFace.Ace} of {CardSuit.Clubs} {CardFace.Eight} of {CardSuit.Diamonds}");
+
+            var listOfCard = new List<ICard>();
+            listOfCard.Add(firstCard);
+            listOfCard.Add(secondCard);
+
+            var hand = new Hand(listOfCard);
+            var actualResult = hand.ToString();
+
+            Assert.AreNotEqual(expectedResult, actualResult);
+        }
+    }
+}
