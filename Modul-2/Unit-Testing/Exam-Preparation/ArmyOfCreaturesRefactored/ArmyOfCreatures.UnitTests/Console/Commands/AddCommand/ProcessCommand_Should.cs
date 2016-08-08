cb97df593b0ebf48e6d2ab787e22909f852808e5
owolp <zkostov@protonmail.ch>
2016-08-08 23:05:23 +0300

    ACR

---
+++ Modul-2/Unit-Testing/Exam-Preparation/ArmyOfCreaturesRefactored/ArmyOfCreatures.UnitTests/Console/Commands/AddCommand/ProcessCommand_Should.cs
@@ -0,0 +1,52 @@
+﻿namespace ArmyOfCreatures.UnitTests.Console.Commands.AddCommand
+{
+    using System;
+    using ArmyOfCreatures.Console.Commands;
+    using NUnit.Framework;
+    using Moq;
+    using ArmyOfCreatures.Logic.Battles;
+
+    [TestFixture]
+    public class ProcessCommand_Should
+    {
+        [Test]
+        public void ThrowArgumentNullExpeption_WhenTheIBattleManagerIsNull()
+        {
+            var addCommand = new AddCommand();
+            IBattleManager battleManager = null;
+            string[] arguments = new string[] { "10", "AncientBehemoth(1)" };
+
+            Assert.Throws<ArgumentNullException>(() => addCommand.ProcessCommand(battleManager, arguments));
+        }
+
+        [Test]
+        public void ThrowArgumentNullExpeption_WhenTheParamsIsNull()
+        {
+            var addCommand = new AddCommand();
+            var mockedBattleManager = new Mock<IBattleManager>();
+            string[] arguments = null;
+
+            Assert.Throws<ArgumentNullException>(() => addCommand.ProcessCommand(mockedBattleManager.Object, arguments));
+        }
+
+        [Test]
+        public void ThrowArgumentNullExpeption_WhenTheParamsArgumentIsInavlid()
+        {
+            var addCommand = new AddCommand();
+            var mockedBattleManager = new Mock<IBattleManager>();
+            string[] arguments = new string[] { "10" };
+
+            Assert.Throws<ArgumentException>(() => addCommand.ProcessCommand(mockedBattleManager.Object, arguments));
+        }
+
+        [Test]
+        public void ReturnNoExpection_WhenTheCommandIsParsedSuccessfully()
+        {
+            var addCommand = new AddCommand();
+            var mockedBattleManager = new Mock<IBattleManager>();
+            string[] arguments = new string[] { "10", "AncientBehemoth(1)" };
+
+            Assert.DoesNotThrow(() => addCommand.ProcessCommand(mockedBattleManager.Object, arguments));
+        }
+    }
+}
---
+++ Modul-2/Unit-Testing/Exam-Preparation/ArmyOfCreaturesRefactored/ArmyOfCreatures.UnitTests/Logic/Specialities/DoubleDefenseWhenDefending/ApplyWhenDefending_Should.cs
@@ -0,0 +1,73 @@
+﻿namespace ArmyOfCreatures.UnitTests.Logic.Specialities.DoubleDefenseWhenDefending
+{
+    using System;
+    using ArmyOfCreatures.Logic.Battles;
+    using ArmyOfCreatures.Logic.Specialties;
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ApplyWhenDefending_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenTheICreaturesInBattleDefenderWithSpecialtyIsNull()
+        {
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(10);
+            ICreaturesInBattle defenderWithSpecialty = null;
+            var mockedAttacker = new Mock<ICreaturesInBattle>();
+
+            Assert.Throws<ArgumentNullException>(() => doubleDefenseWhenDefending.ApplyWhenDefending(defenderWithSpecialty, mockedAttacker.Object));
+        }
+
+        [Test]
+        public void ThrowArgumentNullException_WhenTheICreaturesInBattleAttackedIsNull()
+        {
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(10);
+            var mockedDefenderWithSpeciality = new Mock<ICreaturesInBattle>();
+            ICreaturesInBattle attacker = null;
+
+            Assert.Throws<ArgumentNullException>(() => doubleDefenseWhenDefending.ApplyWhenDefending(mockedDefenderWithSpeciality.Object, attacker));
+        }
+
+        [Test]
+        public void ReturnWithoutChangingTheCurrentDefensePropertyOfDefenderWithSpecialty_WhenTheEffectIsExpired()
+        {
+            int rounds = 5;
+            int roundsWithEffectExpired = 10;
+
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(rounds);
+            var mockedDefenderWithSpeciality = new Mock<ICreaturesInBattle>();
+            var mockedAttacker = new Mock<ICreaturesInBattle>();
+
+            mockedDefenderWithSpeciality.SetupGet(x => x.CurrentDefense).Returns(It.IsAny<int>());
+
+            for (int i = 0; i < rounds + roundsWithEffectExpired; i++)
+            {
+                doubleDefenseWhenDefending.ApplyWhenDefending(mockedDefenderWithSpeciality.Object, mockedAttacker.Object);
+            }
+
+            mockedDefenderWithSpeciality.VerifySet(x => x.CurrentDefense = It.IsAny<int>(), Times.Exactly(rounds));
+        }
+
+        [Test]
+        public void ReturnShouldMultiplyTheCurrentDefensePropertyOfDefenderWithSpecialtyByTwo_WhenTheEffectHasNotExpired()
+        {
+            int defence = 1;
+            int rounds = 5;
+            int roundsWithEffectExpired = 10;
+
+            var doubleDefenseWhenDefending = new DoubleDefenseWhenDefending(rounds);
+            var mockedDefenderWithSpeciality = new Mock<ICreaturesInBattle>();
+            var mockedAttacker = new Mock<ICreaturesInBattle>();
+
+            mockedDefenderWithSpeciality.SetupGet(x => x.CurrentDefense).Returns(defence);
+
+            for (int i = 0; i < rounds + roundsWithEffectExpired; i++)
+            {
+                doubleDefenseWhenDefending.ApplyWhenDefending(mockedDefenderWithSpeciality.Object, mockedAttacker.Object);
+            }
+
+            mockedDefenderWithSpeciality.VerifySet(x => x.CurrentDefense = It.Is<int>(y => y == defence * 2), Times.Exactly(rounds));
+        }
+    }
+}
