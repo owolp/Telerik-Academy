fecc67e29472252a3f3aa6f4eb154da666140a35
owolp <zkostov@protonmail.ch>
2016-08-08 11:31:55 +0300

    Update ArmyOfCreatures

--- Modul-2/Unit-Testing/Exam-Preparation/ArmyOfCreatures/ArmyOfCreatures.UnitTests/Logic/Battles/BattleManager/AddCreatures_Should.cs
+++ Modul-2/Unit-Testing/Exam-Preparation/ArmyOfCreatures/ArmyOfCreatures.UnitTests/Logic/Battles/BattleManager/AddCreatures_Should.cs
@@ -40,5 +40,32 @@
 
             mockedCreaturesFactory.Verify(x => x.CreateCreature(It.IsAny<string>()), Times.Once);
         }
+
+        // var creaturesInBattle = new CreaturesInBattle(creature, count);
+        // This should work, as creature has been tested
+        // I think that count should be tested as well
+
+        // this.AddCreaturesByIdentifier(creatureIdentifier, creaturesInBattle);
+        // Do not test - as mocking the object under test is not allowed
+
+        [Test]
+        public void CallTheLogger_WhenTheCreatureIdentifierIsValid()
+        {
+            var mockedCreaturesFactory = new Mock<ICreaturesFactory>();
+            var mockedLogger = new Mock<ILogger>();
+
+            mockedLogger.Setup(x => x.WriteLine(It.IsAny<string>()));
+
+            var battleManager = new BattleManager(mockedCreaturesFactory.Object, mockedLogger.Object);
+
+            var creatuerIdentifier = CreatureIdentifier.CreatureIdentifierFromString("Angel(1)");
+            var angel = new Angel();
+
+            mockedCreaturesFactory.Setup(x => x.CreateCreature(It.IsAny<string>())).Returns(angel);
+
+            battleManager.AddCreatures(creatuerIdentifier, 1);
+
+            mockedLogger.Verify(x => x.WriteLine(It.IsAny<string>()), Times.Once);
+        }
     }
 }
