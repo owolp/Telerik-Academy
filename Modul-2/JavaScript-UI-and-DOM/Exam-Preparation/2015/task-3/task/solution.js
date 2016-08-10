fa341d91f566663cf440b2ad5e896eaabd813bc9
owolp <zkostov@protonmail.ch>
2016-08-10 17:12:11 +0300

    examprep

---
+++ Modul-2/JavaScript-UI-and-DOM/Exam-Preparation/2015/task-3/task/solution.js
@@ -0,0 +1,37 @@
+function solve() {
+    return function (selector) {
+        var template = [
+'<div id="calendar">' +
+'<div class="events-calendar">' +
+'<h2 class="header">' +
+'Appointments for <span class="month">{{month}}</span> <span class="year">{{year}}</span>' +
+'</h2>' +
+'{{#days}}' +
+'<div class="col-date">' +
+'<div class="date">{{day}}</div>' +
+'<div class="events">' +
+'{{#events}}' +
+'<div class="event {{importance}}">' +
+'{{#if title}}' +
+'<div class="title">{{title}}</div>' +
+'<span class="time">at: {{time}}</span>' +
+'{{else}}' +
+'<div class="title">Free slot</div>' +
+'{{/if}}' +
+'</div>' +
+'{{/events}}' +
+'</div>' +
+'</div>' +
+'{{/days}}' +
+'</div>'
+        ].join('');
+
+        if(template.length) {
+            document.getElementById(selector).innerHTML = template;
+        }
+    };
+}
+
+if (typeof module !== 'undefined') {
+    module.exports = solve;
+}
--- Modul-2/JavaScript-UI-and-DOM/Exam-Preparation/README.md
+++ Modul-2/JavaScript-UI-and-DOM/Exam-Preparation/README.md
@@ -4,4 +4,6 @@
 | :-: | :-: | :-: | :-: |
 | 2015 | Items control | Datepicker | [Calendar template](./2015/task-3) |
 | 2014 | Image Preview | Gallery | [Trainers List template](./2014/task-3) |
-| Sample Exam | Calendar | Tabs | Books |
+| Sample Exam | Calendar | Tabs | [Books](Sample-Exam\task3) |
+| Worskhop 2016 | | | [Calendar template](Workshop-2016\calendar-template) |
+| Worskhop 2016 | | | [List template](Workshop-2016\list-template) |
