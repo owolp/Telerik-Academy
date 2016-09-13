09822f392d763ec7ad07af16e8ec3086a3ae60c7
owolp <zkostov@protonmail.ch>
2016-09-13 23:01:13 +0300

    refactor hqc2 hw01

---
+++ Module-2/HQC-Part-2/Homework/01-Defensive-Programming-And-Exceptions/Assertions-ConsoleClient/Startup.cs
@@ -0,0 +1,25 @@
+﻿namespace Assertions_ConsoleClient
+{
+    using System;
+    using Assertions;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            //int[] arr = new int[] { 3, -1, 15, 4, 17, 2, 33, 0 };
+            //Console.WriteLine("arr = [{0}]", string.Join(", ", arr));
+            //SelectionSort(arr);
+            //Console.WriteLine("sorted = [{0}]", string.Join(", ", arr));
+
+            //SelectionSort(new int[0]); // Test sorting empty array
+            //SelectionSort(new int[1]); // Test sorting single element array
+
+            //Console.WriteLine(BinarySearch(arr, -1000));
+            //Console.WriteLine(BinarySearch(arr, 0));
+            //Console.WriteLine(BinarySearch(arr, 17));
+            //Console.WriteLine(BinarySearch(arr, 10));
+            //Console.WriteLine(BinarySearch(arr, 1000));
+        }
+    }
+}
---
+++ Module-2/HQC-Part-2/Homework/01-Defensive-Programming-And-Exceptions/Assertions-Homework/AssertionsHomework.cs
@@ -0,0 +1,70 @@
+﻿namespace Assertions
+{
+    using System;
+
+    public class AssertionsHomework
+    {
+        public static void SelectionSort<T>(T[] arr) where T : IComparable<T>
+        {
+            for (int index = 0; index < arr.Length - 1; index++)
+            {
+                int minElementIndex = FindMinElementIndex(arr, index, arr.Length - 1);
+                Swap(ref arr[index], ref arr[minElementIndex]);
+            }
+        }
+
+        public static int BinarySearch<T>(T[] arr, T value) where T : IComparable<T>
+        {
+            return BinarySearch(arr, value, 0, arr.Length - 1);
+        }
+
+        private static int FindMinElementIndex<T>(T[] arr, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            int minElementIndex = startIndex;
+            for (int i = startIndex + 1; i <= endIndex; i++)
+            {
+                if (arr[i].CompareTo(arr[minElementIndex]) < 0)
+                {
+                    minElementIndex = i;
+                }
+            }
+
+            return minElementIndex;
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            T oldX = x;
+            x = y;
+            y = oldX;
+        }
+
+        private static int BinarySearch<T>(T[] arr, T value, int startIndex, int endIndex)
+            where T : IComparable<T>
+        {
+            while (startIndex <= endIndex)
+            {
+                int midIndex = (startIndex + endIndex) / 2;
+                if (arr[midIndex].Equals(value))
+                {
+                    return midIndex;
+                }
+
+                if (arr[midIndex].CompareTo(value) < 0)
+                {
+                    // Search on the right half
+                    startIndex = midIndex + 1;
+                }
+                else
+                {
+                    // Search on the right half
+                    endIndex = midIndex - 1;
+                }
+            }
+
+            // Searched value not found
+            return -1;
+        }
+    }
+}
