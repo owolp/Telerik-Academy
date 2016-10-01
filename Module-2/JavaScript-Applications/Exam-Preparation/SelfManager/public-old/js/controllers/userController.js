10297d81d1a9bdef621160c99c872675026fe30c
owolp <zkostov@protonmail.ch>
2016-10-01 18:02:02 +0300

    last update

--- Module-2/JavaScript-Applications/Exam-Preparation/SelfManager/public-old/js/controllers/userController.js
+++ Module-2/JavaScript-Applications/Exam-Preparation/SelfManager/public-old/js/controllers/userController.js
@@ -4,31 +4,6 @@ var userController = function () {
 		templates.get('register')
 			.then((templateHtml) => {
 				$('#container').html(templateHtml);
-
-				// attach button events
-				$('#btn-login').on('click', function () {
-					let user = {
-						username: $('#tb-username').val(),
-						password: $('#tb-password').val()
-					};
-					data.login(user)
-						.then(function () {
-							toastr.success(`User ${user.username} logged in`);
-							document.location = '#/';
-						})
-				});
-
-				// attach button events
-				$('#btn-register').on('click', function () {
-					let user = {
-						username: $('#tb-username').val(),
-						password: $('#tb-password').val()
-					};
-					data.register(user)
-						.then(function () {
-							console.log('User registered');
-						})
-				});
 			});
 	}
 
--- Module-2/JavaScript-Applications/Exam-Preparation/SelfManager/public/controllers/loginController.js
+++ Module-2/JavaScript-Applications/Exam-Preparation/SelfManager/public/controllers/loginController.js
@@ -4,7 +4,34 @@ var loginController = function () {
 		templates.load('login')
 			.then((templateHtml) => {
 				$('#container').html(templateHtml);
-			});		
+			})
+			.then(() => {
+				$('#btn-login').on('click', function () {
+					let user = {
+						username: $('#tb-username').val(),
+						password: $('#tb-password').val()
+					};
+					userservice.login(user)
+						.then(function () {
+							toastr.success(`User ${user.username} logged in`);
+						})
+				});
+
+				// attach button events
+				$('#btn-register').on('click', function () {
+					let user = {
+						username: $('#tb-username').val(),
+						password: $('#tb-password').val()
+					};
+					userservice.register(user)
+						.then(function () {
+							console.log('User registered');
+						})
+				});
+			});
+
+		// attach button events
+
 	}
 
 	return {
