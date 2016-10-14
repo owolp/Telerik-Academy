792ebf0a46793b6b194ddb07c2dc4cd9f278b0ea
owolp <zkostov@protonmail.ch>
2016-10-14 15:07:57 +0300

    part of db hw02

---
+++ Module-2/Databases/Homework/02-Processing-XML-in-.NET/02.ExtractUsingDom/ExtractUsingDom.cs
@@ -0,0 +1,76 @@
+﻿namespace ExtractUsingDom
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Xml;
+
+    public class ExtractUsingDom
+    {
+        private const string documentLocation = "../../catalog.xml";
+
+        public static void Main()
+        {
+            var rootNode = LoadDocument();
+            var artists = GetAllArtists(rootNode);
+            var albums = ExtractAlbumsForArtist(rootNode);
+            var output = Output(albums);
+
+            Console.Write(output);
+        }
+
+        private static XmlElement LoadDocument()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(documentLocation);
+            XmlElement rootNode = doc.DocumentElement;
+
+            return rootNode;
+        }
+
+        private static StringBuilder Output(IDictionary<string, int> albums)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                var output = $"{album.Key} - {album.Value}";
+                result.AppendLine(output);
+            }
+
+            return result;
+        }
+
+        private static IDictionary<string, int> ExtractAlbumsForArtist(XmlElement rootNode)
+        {
+            var result = new Dictionary<string, int>();
+
+            var albums = rootNode.GetElementsByTagName("cat:album");
+            foreach (XmlNode album in albums)
+            {
+                var artist = album["cat:artist"].InnerText;
+                if (!result.ContainsKey(artist))
+                {
+                    result[artist] = 0;
+                }
+
+                result[artist]++;
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<string> GetAllArtists(XmlElement rootNode)
+        {
+            var result = new HashSet<string>();
+
+            var albums = rootNode.GetElementsByTagName("cat:album");
+            foreach (XmlNode album in albums)
+            {
+                result.Add(album["cat:artist"].InnerText);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Module-2/Databases/Homework/02-Processing-XML-in-.NET/03.ExtractUsingXPath/ExtractUsingXPath.cs
@@ -0,0 +1,63 @@
+﻿namespace ExtractUsingXPath
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Xml;
+
+    public class ExtractUsingXPath
+    {
+        private const string documentLocation = "../../catalog.xml";
+
+        public static void Main()
+        {
+            var rootNode = LoadDocument();
+            string xPathQuery = "/catalog/album";
+            var albumsList = rootNode.SelectNodes(xPathQuery);
+            var artistList = ExtractArtistsFromCatalog(albumsList);
+            var output = Output(artistList);
+
+            Console.Write(output);
+        }
+
+        private static XmlElement LoadDocument()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(documentLocation);
+            XmlElement rootNode = doc.DocumentElement;
+
+            return rootNode;
+        }
+
+        private static StringBuilder Output(IDictionary<string, int> albums)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var album in albums)
+            {
+                var output = $"{album.Key} - {album.Value}";
+                result.AppendLine(output);
+            }
+
+            return result;
+        }
+
+        private static Dictionary<string, int> ExtractArtistsFromCatalog(IEnumerable albumsList)
+        {
+            var artistsList = new Dictionary<string, int>();
+            foreach (XmlNode album in albumsList)
+            {
+                var artistName = album.SelectSingleNode("artist").InnerText;
+                if (!artistsList.ContainsKey(artistName))
+                {
+                    artistsList[artistName] = 0;
+                }
+
+                artistsList[artistName]++;
+            }
+
+            return artistsList;
+        }
+    }
+}
