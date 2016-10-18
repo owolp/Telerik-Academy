47774d16304fb360cc63ff5cb2284bd64fe45806
owolp <zkostov@protonmail.ch>
2016-10-18 16:53:17 +0300

    up to 14

--- Module-2/Databases/Homework/08-Advanced-SQL/SQLQuery.sql
+++ Module-2/Databases/Homework/08-Advanced-SQL/SQLQuery.sql
@@ -69,17 +69,109 @@ SELECT
 	COUNT(*) AS [EmployeesCount]
 FROM Employees e
 INNER JOIN Departments d
-ON e.DepartmentID = d.DepartmentID
+	ON e.DepartmentID = d.DepartmentID
 WHERE d.Name = 'Sales'
 
 -- ====================================================================================================
+
+-- 7. Write a SQL query to find the number of all employees that have manager.
+
+SELECT
+	COUNT(*) AS [EmployeesWithManager]
+FROM Employees e
+WHERE e.ManagerID IS NOT NULL
+
 -- ====================================================================================================
+
+-- 8. Write a SQL query to find the number of all employees that have no manager.
+
+SELECT
+	COUNT(*) AS [EmployeesWithManager]
+FROM Employees e
+WHERE e.ManagerID IS NULL
+
 -- ====================================================================================================
+
+-- 9. Write a SQL query to find all departments and the average salary for each of them.
+
+SELECT
+	d.Name,
+	ROUND(AVG(e.Salary), 2) AS [AverageSalary]
+FROM	Departments d,
+		Employees e
+WHERE e.DepartmentID = d.DepartmentID
+GROUP BY d.Name
+ORDER BY AverageSalary DESC
+
 -- ====================================================================================================
+
+-- 10. Write a SQL query to find the count of all employees in each department and for each town.
+
+SELECT
+	d.Name AS [DepartmentName],
+	t.Name AS [TownName],
+	COUNT(*) AS [EmployeesCount]
+FROM	Departments d,
+		Employees e,
+		Towns t,
+		Addresses a
+WHERE e.DepartmentID = d.DepartmentID
+AND e.AddressID = a.AddressID
+AND a.TownID = t.TownID
+GROUP BY	t.Name,
+			d.Name
+
 -- ====================================================================================================
+
+-- 11. Write a SQL query to find all managers that have exactly 5 employees. Display their first name and last name.
+
+SELECT
+	m.FirstName + ' ' + m.LastName AS [ManagerName]
+FROM	Employees e,
+		Employees m
+WHERE e.ManagerID = m.EmployeeID
+GROUP BY	m.FirstName,
+			m.LastName
+HAVING COUNT(*) = 5
+
 -- ====================================================================================================
+
+-- 12. Write a SQL query to find all employees along with their managers. For employees that do not have manager display the value "(no manager)".
+
+SELECT
+	e.FirstName + ' ' + e.LastName AS [EmployeeFullName],
+	ISNULL(m.FirstName + ' ' + m.LastName, 'no manager') AS [ManagerFullName]
+FROM Employees e
+LEFT OUTER JOIN Employees m
+	ON e.ManagerID = m.EmployeeID
+
+-- Managers without direct reports
+
+SELECT
+	m.FirstName + ' ' + m.LastName AS [ManagerFullName],
+	ISNULL(e.FirstName + ' ' + e.LastName, 'no reports') AS [EmployeeFullName]
+FROM Employees m
+LEFT OUTER JOIN Employees e
+	ON e.ManagerID = m.EmployeeID
+ORDER BY M.FirstName
+
 -- ====================================================================================================
+
+-- 13. Write a SQL query to find the names of all employees whose last name is exactly 5 characters long. Use the built-in LEN(str) function.
+
+SELECT
+	e.FirstName,
+	e.LastName
+FROM Employees e
+WHERE LEN(e.LastName) = 5
+ORDER BY e.LastName
+
 -- ====================================================================================================
+
+-- 14. Write a SQL query to display the current date and time in the following format "day.month.year hour:minutes:seconds:milliseconds".
+
+SELECT FORMAT(GETDATE(), 'dd.MM.yyyy HH:mm:ss:fff')
+
 -- ====================================================================================================
 -- ====================================================================================================
 -- ====================================================================================================
