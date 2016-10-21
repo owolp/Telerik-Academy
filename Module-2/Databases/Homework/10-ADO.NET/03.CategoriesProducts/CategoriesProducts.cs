352c9af45f8e4393c809c5dfa63a2373ce290f11
owolp <zkostov@protonmail.ch>
2016-10-21 21:00:35 +0300

    add task 3

---
+++ Module-2/Databases/Homework/10-ADO.NET/03.CategoriesProducts/CategoriesProducts.cs
@@ -0,0 +1,35 @@
+﻿namespace CategoriesProducts
+{
+    using System;
+    using System.Data.SqlClient;
+
+    internal class CategoriesProducts
+    {
+        private static void Main()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=.; Database=Northwind; Integrated Security = true");
+            sqlConnection.Open();
+
+            using (sqlConnection)
+            {
+                SqlCommand command = new SqlCommand(
+                    "SELECT p.ProductName AS [Product], " + 
+                    "c.CategoryName AS [Category] FROM Products p " + 
+                    "JOIN Categories c ON p.CategoryID = c.CategoryID",
+                    sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string categoryName = (string)reader["Product"];
+                        string products = (string)reader["Category"];
+                        Console.WriteLine($"Product: {categoryName}");
+                        Console.WriteLine($"Category: {products}");
+                        Console.WriteLine(new string('-', 20));
+                    }
+                }
+            }
+        }
+    }
+}
