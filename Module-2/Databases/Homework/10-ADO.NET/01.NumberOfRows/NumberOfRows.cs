d48eb1d7eba1b8aa8d848b0abcda576f44c2553b
owolp <zkostov@protonmail.ch>
2016-10-21 20:44:08 +0300

    ado hw up to 2

---
+++ Module-2/Databases/Homework/10-ADO.NET/01.NumberOfRows/NumberOfRows.cs
@@ -0,0 +1,21 @@
+﻿namespace NumberOfRows
+{
+    using System;
+    using System.Data.SqlClient;
+
+    internal class NumberOfRows
+    {
+        private static void Main()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=.; Database=Northwind; Integrated Security = true");
+            sqlConnection.Open();
+
+            using (sqlConnection)
+            {
+                SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Categories", sqlConnection);
+                int categoriesCount = (int)command.ExecuteScalar();
+                Console.WriteLine("Categories count: {0}", categoriesCount);
+            }
+        }
+    }
+}
---
+++ Module-2/Databases/Homework/10-ADO.NET/02.InformationAboutCategories/InformationAboutCategories.cs
@@ -0,0 +1,31 @@
+﻿namespace InformationAboutCategories
+{
+    using System;
+    using System.Data.SqlClient;
+
+    internal class InformationAboutCategories
+    {
+        private static void Main()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Server=.; Database=Northwind; Integrated Security = true");
+            sqlConnection.Open();
+
+            using (sqlConnection)
+            {
+                SqlCommand command = new SqlCommand("SELECT * FROM Categories", sqlConnection);
+                SqlDataReader reader = command.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        string categoryName = (string)reader["CategoryName"];
+                        string description = (string)reader["Description"];
+                        Console.WriteLine($"Category name: {categoryName}");
+                        Console.WriteLine($"Description: {description}");
+                        Console.WriteLine(new string('-', 20));
+                    }
+                }
+            }
+        }
+    }
+}
