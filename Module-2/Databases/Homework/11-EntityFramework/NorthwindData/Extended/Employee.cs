d714c2dada4560932523ad5914f92b361e1f602d
owolp <zkostov@protonmail.ch>
2016-10-29 17:11:18 +0300

    Add entity hw

---
+++ Module-2/Databases/Homework/11-EntityFramework/NorthwindData/Extended/Employee.cs
@@ -0,0 +1,18 @@
+﻿namespace NorthwindData
+{
+    using System.Data.Linq;
+
+    public partial class Employee
+    {
+        public EntitySet<Territory> CorrespondingTerritories
+        {
+            get
+            {
+                var result = new EntitySet<Territory>();
+                result.Assign(this.Territories);
+
+                return result;
+            }
+        }
+    }
+}
